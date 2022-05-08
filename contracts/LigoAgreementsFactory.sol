e4619eda1aeee6d930a51528756e85f00052876b
Lenovo <benasvol@gmail.com>
2022-05-08 19:52:43 +0300

    Add rental agreement contract flow

--- contracts/LigoAgreementsFactory.sol
+++ contracts/LigoAgreementsFactory.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
-contract LigoContract is Ownable {    
+contract LigoAgreementsFactory is Ownable {
+
+    /**
+     * @dev Return a vehicle ID for a given vehicle address
+     */  
+    function getVehicleId(address _vehicleAddress) public view returns (uint) {
+        // TODO: update return value
+        return uint(1);
+    }    
 
 }
